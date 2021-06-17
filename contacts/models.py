e62c19378e0c7b2b03487b7b9ec5ac0364a77ff5
smilewendy7 <smilewendy7@gmail.com>
2021-06-16 21:44:04 -0400

    assignment 1 compelete

--- contacts/models.py
+++ contacts/models.py
@@ -19,3 +19,5 @@ class Contact(models.Model):
     city = models.CharField(max_length=255, null=True, blank=True)
     state = USStateField(null=True, blank=True)
     zip_code = USZipCodeField(null=True, blank=True)
+    ## add birthday
+    birthday= models.DateField(null=True, blank=True)
